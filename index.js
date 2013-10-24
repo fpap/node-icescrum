79e3e096a19a14425361b344b63159114f9f371a
fpap <fpap@sursoftware.com.ar>
2013-10-24 11:45:03 -0300

    Method updateStory completed with optional parameters. Method createStory added.

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ var http = require("http");
 var icescrum = {};
     
 /**
- * Gets an story
+ * Gets an story.
  *
- * @method getStory
- * @param {number} story: Required story's id
- * @param {string} hostname: Icescrum's server's host name
- * @param {number} port: Icescrum's server's port
- * @param {string} path: Icescrum's server's path
- * @param {string} project: Icescrum's project's key
+ * @method getStory.
+ * @param {number} story: Required story's id.
+ * @param {string} hostname: Icescrum's server's host name.
+ * @param {number} port: Icescrum's server's port.
+ * @param {string} path: Icescrum's server's path.
+ * @param {string} project: Icescrum's project's key.
  * @return {object} Returns by callback's function:
- *   First value {boolean}: error
+ *   First value {boolean}: error.
  *   Second value {string}:
- *     If first value == true, error's description
+ *     If first value == true, error's description.
  *     If first value == false, required story. Example:
  *       {
  *         "id":750,
@@ -64,6 +64,7 @@ icescrum.getStory = function(params, callback) {
     }
     
     var options = {};
+    
     options.hostname = params.hostname;
     options.port = params.port;
     options.path = '/'+params.path+'/ws/p/'+params.project+'/story/'+params.story;
@@ -91,18 +92,18 @@ icescrum.getStory = function(params, callback) {
 
 
 /**
- * Gets all the stories of a project
+ * Gets all the stories of a project.
  *
- * @method getAllStories
- * @param {string} hostname: Icescrum's server's host name
- * @param {number} port: Icescrum's server's port
- * @param {string} path: Icescrum's server's path
- * @param {string} project: Icescrum's project's key
- * @param {string} auth: Authentication in format user:password
+ * @method getAllStories.
+ * @param {string} hostname: Icescrum's server's host name.
+ * @param {number} port: Icescrum's server's port.
+ * @param {string} path: Icescrum's server's path.
+ * @param {string} project: Icescrum's project's key.
+ * @param {string} auth: Authentication in format user:password.
  * @return {object} Returns by callback's function:
- *   First value {boolean}: error
+ *   First value {boolean}: error.
  *   Second value {string}:
- *     If first value == true, error's description
+ *     If first value == true, error's description.
  *     If first value == false, required story. Example:
  *       [
  *         {
@@ -144,6 +145,7 @@ icescrum.getStory = function(params, callback) {
  */
 icescrum.getAllStories = function(params, callback) {
     var options = {};
+    
     options.hostname = params.hostname;
     options.port = params.port;
     options.path = '/'+params.path+'/ws/p/'+params.project+'/story/';
@@ -172,15 +174,28 @@ icescrum.getAllStories = function(params, callback) {
 
 
 /**
- * Updates an story
+ * Updates an story.
  *
- * @method updateStory
- * @param {number} story: Required story's id
- * @param {string} hostname: Icescrum's server's host name
- * @param {number} port: Icescrum's server's port
- * @param {string} path: Icescrum's server's path
- * @param {string} project: Icescrum's project's key
- * @param {string} auth: Authentication in format user:password
+ * @method updateStory.
+ * @param {number} story: Required story's id.
+ * @param {string} hostname: Icescrum's server's host name.
+ * @param {number} port: Icescrum's server's port.
+ * @param {string} path: Icescrum's server's path.
+ * @param {string} project: Icescrum's project's key.
+ * @param {string} auth: Authentication in format user:password.
+ * @param {string} name: (optional) New story's name.
+ * @param {number} type: (optional) New story's type. 0: user story, 2: defect, 3: technical story.
+ * @param {string} description: (optional) New story's description.
+ * @param {string} notes: (optional) New story's notes. Textile markup allowed.
+ * @param {string} textAs: (optional) New story's "As a" text.  If an actor match the textAs value, it will be linked to the story.
+ * @param {string} textICan: (optional) New story's "I want" text.
+ * @param {string} textTo: (optional) New story's "In order to" text.
+ * @param {number} featureId: (optional) New story's feature id. 
+ * @param {number} sprintId: (optional) New story's sprint id.
+ * @param {number} rank: (optional) New story's rank (relative position in the stories list).
+ * @param {number} effort: (optional) New story's effort.
+ * @param {number} dependsOnId: (optional) New story's depends on id.
+ * @param {string} tags: (optional) New story's tags. Use comma for multiple tags. // TODO: Currently not working.
  * @return {object} Returns by callback's function:
  *   First value {boolean}: error
  *   Second value {string}:
@@ -304,17 +319,240 @@ icescrum.updateStory = function(params, callback) {
         
         sprintId = "sprint: {id: "+params.sprintId+"},";
     }
+    
+    var rank = "";
+    if (typeof params.rank !== 'undefined' && params.rank !== null) {
+        if (params.rank !== parseInt(params.rank, 10)){
+            callback(true, 'Undefined rank');
+            return(this);
+        }
+        else {
+            if (params.rank < 0){
+                callback(true, 'Invalid rank');
+                return(this);
+            }
+        }
+        
+        rank = "rank: "+params.rank+",";
+    }
+    
+    var effort = "";
+    if (typeof params.effort !== 'undefined' && params.effort !== null) {
+        if (params.effort !== parseInt(params.effort, 10)){
+            callback(true, 'Undefined effort');
+            return(this);
+        }
+        else {
+            if (params.effort < 0){
+                callback(true, 'Invalid effort');
+                return(this);
+            }
+        }
+        
+        effort = "effort: "+params.effort+",";
+    }
+    
+    var dependsOnId = "";
+    if (typeof params.dependsOnId !== 'undefined' && params.dependsOnId !== null) {
+        if (params.dependsOnId !== parseInt(params.dependsOnId, 10)){
+            callback(true, 'Undefined depends on');
+            return(this);
+        }
+        else {
+            if (params.dependsOnId < 0){
+                callback(true, 'Invalid depends on');
+                return(this);
+            }
+        }
+        
+        dependsOnId = "dependsOn: {id: "+params.dependsOnId+"},";
+    }
+    
+    // TODO: Fix. Currently not working.
+    var tags = "";
+    if (typeof params.tags !== 'undefined' && params.tags !== null)
+        tags = "tags: ['"+params.tags+"'],";
 
-    var data = "{story: {"+name+type+description+notes+textAs+textICan+textTo+featureId+sprintId+"}}"
+    var data = "{story: {"+name+type+description+notes+textAs+textICan+textTo+featureId+sprintId+rank+effort+dependsOnId+tags+"}}"
     ,options = {};
-
-    //data = "{story: {feature: {id: 91}}}";
     
     options.hostname = params.hostname;
     options.port = params.port;
     options.path = '/'+params.path+'/ws/p/'+params.project+'/story/'+params.story;
     options.auth = params.auth;
     options.method = 'PUT';
+    options.headers = {
+            'content-type': 'application/json'
+            ,'Content-Length': Buffer.byteLength(data)
+    };
+  
+    var req = http.request(options, function(result) {
+        result.setEncoding('utf8');
+        result.on('data', function (chunk) {
+            if (chunk.substr(2,5) == "error") {
+                callback(true, chunk.substr(10,chunk.length - 12));
+                return(this);
+            }
+            else {
+                callback(false, chunk);
+                return(this);
+            }
+        });
+    });
+  
+    req.write(data, 'utf8');
+    req.end();
+};
+
+/**
+ * Creates an story.
+ *
+ * @method createStory.
+ * @param {string} hostname: Icescrum's server's host name.
+ * @param {number} port: Icescrum's server's port.
+ * @param {string} path: Icescrum's server's path.
+ * @param {string} project: Icescrum's project's key.
+ * @param {string} auth: Authentication in format user:password.
+ * @param {string} name: New story's name.
+ * @param {number} type: (optional) New story's type. 0: user story, 2: defect, 3: technical story.
+ * @param {string} description: (optional) New story's description.
+ * @param {string} notes: (optional) New story's notes. Textile markup allowed.
+ * @param {string} textAs: (optional) New story's "As a" text.  If an actor match the textAs value, it will be linked to the story.
+ * @param {string} textICan: (optional) New story's "I want" text.
+ * @param {string} textTo: (optional) New story's "In order to" text.
+ * @param {number} featureId: (optional) New story's feature id. 
+ * @param {number} dependsOnId: (optional) New story's depends on id.
+ * @param {string} tags: (optional) New story's tags. Use comma or spaces for multiple tags.
+ * @return {object} Returns by callback's function:
+ *   First value {boolean}: error
+ *   Second value {string}:
+ *     If first value == true, error's description
+ *     If first value == false, created story. Example:
+ *       {
+ *         "id":750,
+ *         "acceptanceTests":[],
+ *         "acceptedDate":"2013-10-16T14:40:54Z",
+ *         "actor":null,
+ *         "affectVersion":null,
+ *         "creationDate":"2013-10-16T14:40:47Z",
+ *         "creator":{"id":34},
+ *         "dependsOn":null,
+ *         "description":"Updated description",
+ *         "doneDate":null,
+ *         "effort":null,
+ *         "estimatedDate":null,
+ *         "executionFrequency":1,
+ *         "feature":null,
+ *         "inProgressDate":null,
+ *         "lastUpdated":"2013-10-16T14:40:54Z",
+ *         "name":"Test",
+ *         "notes":null,
+ *         "parentSprint":null,
+ *         "plannedDate":null,
+ *         "rank":1,
+ *         "state":2,
+ *         "suggestedDate":"2013-10-16T14:40:47Z",
+ *         "tasks":[],
+ *         "textAs":null,
+ *         "textICan":null,
+ *         "textTo":null,
+ *         "type":0,
+ *         "uid":1,
+ *         "tags":[],
+ *         "dependences":[]
+ *       }
+ */
+icescrum.createStory = function(params, callback) {
+    
+    var name = "";
+    if (typeof params.name !== 'undefined' && params.name !== null)
+        name = "'name': "+params.name+",";
+    else {
+        callback(true, 'Undefined name');
+        return(this);
+    }
+    
+    // Optional params    
+    var type = "";
+    if (typeof params.type !== 'undefined' && params.type !== null) {
+        if (params.type !== parseInt(params.type, 10)){
+            callback(true, 'Undefined type');
+            return(this);
+        }
+        else {
+            if (!(params.type === 0 || params.type == 2 || params.type == 3)){
+                callback(true, 'Invalid type');
+                return(this);
+            }
+        }
+        
+        type = "type: "+params.type+",";
+    }
+    
+    var description = "";
+    if (typeof params.description !== 'undefined' && params.description !== null)
+        description = "description: "+params.description+",";
+    
+    var notes = "";
+    if (typeof params.notes !== 'undefined' && params.notes !== null)
+        notes = "notes: "+params.notes+",";
+    
+    var textAs = "";
+    if (typeof params.textAs !== 'undefined' && params.textAs !== null)
+        textAs = "textAs: "+params.textAs+",";
+    
+    var textICan = "";
+    if (typeof params.textICan !== 'undefined' && params.textICan !== null)
+        textICan = "textICan: "+params.textICan+",";
+    
+    var textTo = "";
+    if (typeof params.textTo !== 'undefined' && params.textTo !== null)
+        textTo = "textTo: "+params.textTo+",";
+    
+    var featureId = "";
+    if (typeof params.featureId !== 'undefined' && params.featureId !== null) {
+        if (params.featureId !== parseInt(params.featureId, 10)){
+            callback(true, 'Undefined feature');
+            return(this);
+        }
+        else {
+            if (params.featureId < 0){
+                callback(true, 'Invalid feature');
+                return(this);
+            }
+        }
+        
+        featureId = "feature: {id: "+params.featureId+"},";
+    }
+    
+    var dependsOnId = "";
+    if (typeof params.dependsOnId !== 'undefined' && params.dependsOnId !== null) {
+        if (params.dependsOnId !== parseInt(params.dependsOnId, 10)){
+            callback(true, 'Undefined depends on');
+            return(this);
+        }
+        else {
+            if (params.dependsOnId < 0){
+                callback(true, 'Invalid depends on');
+                return(this);
+            }
+        }
+        
+        dependsOnId = "dependsOn: {id: "+params.dependsOnId+"},";
+    }
+    
+    var tags = "";
+    if (typeof params.tags !== 'undefined' && params.tags !== null)
+        tags = "tags: ['"+params.tags+"'],";
+
+    var data = "{story: {"+name+type+description+notes+textAs+textICan+textTo+featureId+dependsOnId+tags+"}}"
+    ,options = {};
+    
+    options.hostname = params.hostname;
+    options.port = params.port;
+    options.path = '/'+params.path+'/ws/p/'+params.project+'/story/';
+    options.auth = params.auth;
+    options.method = 'POST';
     options.headers = {
             'content-type': 'application/json'
             ,'Content-Length': Buffer.byteLength(data)
